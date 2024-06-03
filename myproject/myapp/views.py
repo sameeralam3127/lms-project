06b93e198c1e2f88244e90e717586c247c48d9bc
SAMEER ALAM <sameeralam3127@gmail.com>
2024-06-04 00:18:09 +0530

    Updated on 4-6-2024

---
+++ myproject/myapp/views.py
@@ -0,0 +1,64 @@
+<<<<<<< HEAD
+from django.contrib.auth.decorators import login_required  # Import login_required decorator
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from .models import Course
+
+def index(request):
+    return render(request, 'index.html')
+
+@login_required
+def profile(request):
+    return render(request, 'profile.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('login')  # Redirect to index page after successful signup
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+@login_required
+def assigned_courses(request):
+    # Get assigned courses for the current user
+    assigned_courses = request.user.courses.all()
+    
+=======
+from django.contrib.auth.decorators import login_required  # Import login_required decorator
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from .models import Course
+
+def index(request):
+    return render(request, 'index.html')
+
+@login_required
+def profile(request):
+    return render(request, 'profile.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')  # Redirect to index page after successful signup
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+@login_required
+def assigned_courses(request):
+    # Get assigned courses for the current user
+    assigned_courses = request.user.courses.all()
+    
+>>>>>>> edb0d550bb3b14100fe77431cd9e7433f0c5f397
+    return render(request, 'assigned_courses.html', {'assigned_courses': assigned_courses})
---
+++ myproject/myproject/views.py
@@ -0,0 +1,103 @@
+<<<<<<< HEAD
+from django.contrib.auth.decorators import login_required  # Import login_required decorator
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth import login
+
+
+
+
+def feedback(request):
+    return render(request, 'feedback.html')
+
+def indexcourse(request):
+    return render(request, 'course.html')
+
+
+def index(request):
+    return render(request, 'index.html')
+
+@login_required
+def profile(request):
+    return render(request, 'profile.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('login')  # Redirect to login page after successful signup
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+def assigned_courses(request):
+    # Get assigned courses for the current user
+    assigned_courses = request.user.courses.all()
+    
+    return render(request, 'assigned_courses.html', {'assigned_courses': assigned_courses})
+
+@login_required
+def profile_edit(request):
+    if request.method == 'POST':
+        form = UserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')  # Redirect to the profile page after saving changes
+    else:
+        form = UserChangeForm(instance=request.user)
+    return render(request, 'profile_edit.html', {'form': form})
+   
+
+=======
+from django.contrib.auth.decorators import login_required  # Import login_required decorator
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth import login
+
+
+def index(request):
+    return render(request, 'index.html')
+
+@login_required
+def profile(request):
+    return render(request, 'profile.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('login')  # Redirect to login page after successful signup
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+def assigned_courses(request):
+    # Get assigned courses for the current user
+    assigned_courses = request.user.courses.all()
+    
+    return render(request, 'assigned_courses.html', {'assigned_courses': assigned_courses})
+
+@login_required
+def profile_edit(request):
+    if request.method == 'POST':
+        form = UserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')  # Redirect to the profile page after saving changes
+    else:
+        form = UserChangeForm(instance=request.user)
+    return render(request, 'profile_edit.html', {'form': form})
+   
+
+>>>>>>> edb0d550bb3b14100fe77431cd9e7433f0c5f397
+   
