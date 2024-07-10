07ce4fed719b5a7d43b6e789c18aad5f21a61937
Sameer Alam <sameeralam3127@gmail.com>
2024-07-10 23:50:37 +0530

    Updated

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.contrib import admin
 from django.urls import path, include
 from . import views
@@ -19,21 +18,3 @@ urlpatterns = [
     path('feedback/', feedback, name='feedback'),
     path('indexcourse',indexcourse, name="indexcourse")
 ]
-=======
-from django.contrib import admin
-from django.urls import path, include
-from . import views
-from .views import profile, profile_edit
-
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('accounts/', include('django.contrib.auth.urls')),  # Include Django's authentication URLs
-    path('signup/', views.signup, name='signup'),
-    path('accounts/profile/', views.profile, name='profile'),  # Ensure the profile URL is defined like this
-    path('assigned_courses/', views.assigned_courses, name='assigned_courses'),
-    path('profile/', profile, name='profile'),
-    path('profile/edit/', profile_edit, name='profile_edit'),
-]
->>>>>>> edb0d550bb3b14100fe77431cd9e7433f0c5f397
